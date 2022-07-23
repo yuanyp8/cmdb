ffb48f6c30f1d7364baa969f0d68405fcbb7e6bb
gavin.yuan <geniusgavin@163.com>
2022-07-24 02:22:55 +0800

    finish host definition

--- apps/host/model.go
+++ apps/host/model.go
@@ -1,5 +1,12 @@
 package host
 
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"github.com/imdario/mergo"
+	"time"
+)
+
 type Vendor int
 
 const (
@@ -45,4 +52,56 @@ type Describe struct {
 	OSType       string `json:"os_type"`                    // 操作系统类型，分为Windows和Linux
 	OSName       string `json:"os_name"`                    // 操作系统名称
 	SerialNumber string `json:"serial_number"`              // 序列号
+}
+
+type HostSet struct {
+	Total int     `json:"total"`
+	Items []*Host `json:"items"`
+}
+
+func NewHostSet() *HostSet {
+	return &HostSet{
+		Items: []*Host{},
+	}
+}
+
+// Add 将向主机列表增加主机的操作封装成一个函数，使得代码变得优雅
+func (s *HostSet) Add(item *Host) {
+	s.Items = append(s.Items, item)
+}
+
+// Put 对象全量更新
+func (h *Host) Put(obj *Host) error {
+	if obj.Id != h.Id {
+		return fmt.Errorf("id not equal")
+	}
+
+	*h.Resource = *obj.Resource
+	*h.Describe = *obj.Describe
+	return nil
+}
+
+// Patch 对象的局部更新
+func (h *Host) Patch(obj *Host) error {
+	// if obj.Name != "" {
+	// 	h.Name = obj.Name
+	// }
+	// if obj.CPU != 0 {
+	// 	h.CPU = obj.CPU
+	// }
+	// 比如 obj.A  obj.B  只想修改obj.B该属性
+	return mergo.Merge(h, obj)
+}
+
+var validate = validator.New()
+
+// Validate 字段合法性校验
+func (h *Host) Validate() error {
+	return validate.Struct(h)
+}
+
+func (h *Host) InjectDefault() {
+	if h.CreateAt == 0 {
+		h.CreateAt = time.Now().UnixMilli()
+	}
 }
