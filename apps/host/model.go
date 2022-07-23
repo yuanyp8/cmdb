721a82f68b3cfdfcc40718d70d3f45f94df7c56b
gavin.yuan <geniusgavin@163.com>
2022-07-24 01:47:28 +0800

    finish host definition

---
+++ apps/host/model.go
@@ -0,0 +1,48 @@
+package host
+
+type Vendor int
+
+const (
+	// PRIVATE_IDC 枚举的默认值
+	PRIVATE_IDC Vendor = iota
+	// ALIYUN 阿里云
+	ALIYUN
+	// TXYUN 腾讯云
+	TXYUN
+)
+
+// Host 模型的定义
+type Host struct {
+	// 资源公共属性部分
+	*Resource
+	// 资源独有属性部分
+	*Describe
+}
+
+type Resource struct {
+	Id          string            `json:"id"  validate:"required"`     // 全局唯一Id
+	Vendor      Vendor            `json:"vendor"`                      // 厂商
+	Region      string            `json:"region"  validate:"required"` // 地域
+	CreateAt    int64             `json:"create_at"`                   // 创建时间
+	ExpireAt    int64             `json:"expire_at"`                   // 过期时间
+	Type        string            `json:"type"  validate:"required"`   // 规格
+	Name        string            `json:"name"  validate:"required"`   // 名称
+	Description string            `json:"description"`                 // 描述
+	Status      string            `json:"status"`                      // 服务商中的状态
+	Tags        map[string]string `json:"tags"`                        // 标签
+	UpdateAt    int64             `json:"update_at"`                   // 更新时间
+	SyncAt      int64             `json:"sync_at"`                     // 同步时间
+	Account     string            `json:"accout"`                      // 资源的所属账号
+	PublicIP    string            `json:"public_ip"`                   // 公网IP
+	PrivateIP   string            `json:"private_ip"`                  // 内网IP
+}
+
+type Describe struct {
+	CPU          int    `json:"cpu" validate:"required"`    // 核数
+	Memory       int    `json:"memory" validate:"required"` // 内存
+	GPUAmount    int    `json:"gpu_amount"`                 // GPU数量
+	GPUSpec      string `json:"gpu_spec"`                   // GPU类型
+	OSType       string `json:"os_type"`                    // 操作系统类型，分为Windows和Linux
+	OSName       string `json:"os_name"`                    // 操作系统名称
+	SerialNumber string `json:"serial_number"`              // 序列号
+}
---
+++ conf/load.go
@@ -0,0 +1,45 @@
+package conf
+
+import (
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"github.com/caarlos0/env/v6"
+)
+
+var global *Config
+
+func (c *Config) InitGlobal() error {
+	// 后期还会有别的初始化配置，可能会报错，所以这里预留error
+	global = c
+	return nil
+}
+
+// LoadConfigFromToml 从配置文件加载配置
+func LoadConfigFromToml(filepath string) (*Config, error) {
+	cfg := newConfig()
+	_, err := toml.DecodeFile(filepath, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("load config from file error, path:%s, %s", filepath, err)
+	}
+	return cfg, nil
+}
+
+func LoadConfigFromTomlAndENV(filepath string) (*Config, error) {
+	cfg, err := LoadConfigFromToml(filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := env.Parse(cfg); err != nil {
+		return nil, fmt.Errorf("load config from ENV error, %s", err)
+	}
+	return cfg, nil
+}
+
+// C 全局配置对象
+func C() *Config {
+	if global == nil {
+		panic("Load Config first")
+	}
+	return global
+}
